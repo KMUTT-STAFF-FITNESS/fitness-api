3974660d53763777301f6fddc8c770538f932dd9
unknown <thinnapat.2542@mail.kmutt.ac.th>
2020-09-14 17:41:28 +0700

    init DB

---
+++ src/db/migrations/20200914172721_role.js
@@ -0,0 +1,10 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('role', (table) => {
+        table.increments('role_id')
+        table.string('role',50)
+    })
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema.dropTableIfExists('role')
+  };
