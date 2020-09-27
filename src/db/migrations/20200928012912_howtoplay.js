6091c6d968bf6340c72680181719f20452419f3c
sarinrat charoenkunasit <>
2020-09-28 03:00:01 +0700

    ✨[UPDATE]how to play

--- src/db/migrations/20200928012912_howtoplay.js
+++ src/db/migrations/20200928012912_howtoplay.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("howtoplay", (table) => {
     table.increments("howtoplay_id"),
       table.string("machine_group_id", 100),
-      table.string("detail", 500),
+      table.string("detail", 1000),
       table.string("video_path", 250),
       table.string("muscles", 100),
       table.string("note", 100),
