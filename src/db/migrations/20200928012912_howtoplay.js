91e8110b508dfafcdda188e7bd456d7d3be11fe5
unknown <thinnapat.2542@mail.kmutt.ac.th>
2020-09-28 02:31:38 +0700

    [UPDATE] howtoplay schema

--- src/db/migrations/20200928012912_howtoplay.js
+++ src/db/migrations/20200928012912_howtoplay.js
@@ -5,7 +5,10 @@ exports.up = function (knex) {
     table.increments("howtoplay_id"),
       table.string("machine_group_id", 100),
       table.string("detail", 500),
-      table.string("video_path", 250);
+      table.string("video_path", 250),
+      table.string("muscles", 100),
+      table.string("note", 100),
+      table.string("breath", 100);
   });
 };
 
