e4d890e0a9fe3404b4416130f72bb5730d55891e
Thinnapat <mfame2012@gmail.com>
2020-11-20 15:54:31 +0700

    noti db

--- src/db/migrations/20200916162819_NotificationNews.js
+++ src/db/migrations/20200916162819_NotificationNews.js
@@ -1,14 +1,13 @@
-
-exports.up = function(knex) {
-  return knex.schema.createTable('notification_news', (table) => {
-      table.increments('news_id'),
-      table.string('message_title',100),
-      table.string('content', 1000),
-      table.string('create_at',50),
-      table.string('author',100)
-  })
+exports.up = function (knex) {
+  return knex.schema.createTable("notification_news", (table) => {
+    table.increments("news_id"),
+      table.string("message_title", 100),
+      table.string("content", 1000),
+      table.timestamp("create_at").defaultTo(knex.fn.now());
+    table.string("author", 100);
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('notification_news')
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("notification_news");
 };
--- src/db/seeds/NotificationNews.js
+++ src/db/seeds/NotificationNews.js
@@ -1,17 +1,17 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('notification_news').del()
+  return knex("notification_news")
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('notification_news').insert([
+      return knex("notification_news").insert([
         {
-          news_id: '1',
-          message_title: 'ยินดีต้อนรับ',
-          content:'ยินดีต้อนรับสมาชิกใหม่เข้าสู่ฟิตเนสของ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี',
-          create_at:'16/09/2563',
-          author:'Admin_001'
-        }
+          news_id: "1",
+          message_title: "ยินดีต้อนรับ",
+          content:
+            "ยินดีต้อนรับสมาชิกใหม่เข้าสู่ฟิตเนสของ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี",
+          author: "Admin_001",
+        },
       ]);
     });
 };
