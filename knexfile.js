90d29d559273d7fd0a44d6094070f2a87e269c34
Thinnapat <mfame2012@gmail.com>
2020-11-29 04:34:54 +0700

    new db port

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: "localhost",
+      host: "ksf.sit.kmutt.ac.th",
       user: "root",
       password: "root",
       database: "ksf",
-      port: "8889",
+      port: "3306",
       // host:  process.env.HOST_DEV_DB,
       // user: process.env.USER_DEV_DB,
       // password: process.env.PASSWORD_DEV,
